4f51a2bdfd95e0e6273eaf2594a500ba1316029d
Computer13 <computer13@PATC13.local>
2018-03-05 13:02:15 -0800

    March 5 end of class commits

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("connected");
+// console.log("connected");
+//
+// $( document ).ready(function(){
+//   $("#profile-text").css("color", "red");
+//   console.log("jQuery works");
+// });
+
+
+
+// $( document ).ready(function(){
+//   $("#profile-text").click(function(){
+//     $("h1").css("color", "gold");
+// });});
+
+$( document ).ready(function(){
+  var currentColor = $("profile-text").css("color");
+
+  if (currentColor == "rgb(0,0,0)"){
+    $("#profile-text").css("color", "grey");
+  } else {
+    $("#profile-text").css("color", "rgb(0,0,0)");
+  }
+});});

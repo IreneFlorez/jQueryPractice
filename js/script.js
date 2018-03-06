92449ec2e6641a8c31d143414d09d6c607db380f
Computer13 <computer13@PATC13.local>
2018-03-06 12:16:59 -0800

    Tuesday class

--- js/script.js
+++ js/script.js
@@ -5,19 +5,39 @@
 //   console.log("jQuery works");
 // });
 
-
-
 // $( document ).ready(function(){
 //   $("#profile-text").click(function(){
 //     $("h1").css("color", "gold");
 // });});
 
-$( document ).ready(function(){
-  var currentColor = $("profile-text").css("color");
+// $( document ).ready(function(){
+//   var currentColor = $("profile-text").css("color");
+//
+//   if (currentColor == "rgb(0,0,0)"){
+//     $("#profile-text").css("color", "grey");
+//   } else {
+//     $("#profile-text").css("color", "rgb(0,0,0)");
+//   }
+// });});
 
-  if (currentColor == "rgb(0,0,0)"){
-    $("#profile-text").css("color", "grey");
-  } else {
-    $("#profile-text").css("color", "rgb(0,0,0)");
-  }
-});});
+$( document ).ready(function(){
+
+  var box = $("#myBox");
+  var colorString="rgb(";
+
+  $("#buttonColor").click( function (){
+    colorString="rgb(";
+    colorString = colorString +
+    giveMeaRandomNumber() + ", " +
+    giveMeaRandomNumber() + ", " +
+    giveMeaRandomNumber() + ")";
+
+    box.css("background-color", colorString);
+  });
+});
+
+function giveMeaRandomNumber(){
+  var number = Math.random() * 255;
+  number = Math.floor(number);
+  return number;
+}
